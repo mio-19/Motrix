481ef9b6a30b182cdd0c0ba77ee4c7d716b4582f
Taoister39 <Qiacrkeng@gmail.com>
2025-04-03 14:52:47 +0800

    feat: support windows arm64

--- src/main/utils/index.js
+++ src/main/utils/index.js
@@ -179,6 +179,11 @@ export const parseArgvAsFile = (argv) => {
 }
 
 export const getMaxConnectionPerServer = () => {
+  // TODO: must be between 1 and 16 for aria2.exe arm64 version.
+  if (process.platform === 'win32' && process.arch === 'arm64') {
+    return 16
+  }
+
   return ENGINE_MAX_CONNECTION_PER_SERVER
 }
 
